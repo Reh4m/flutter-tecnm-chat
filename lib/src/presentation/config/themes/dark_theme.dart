ba9b8907f905dd6d1f3d855831a9e2c803082a73
emmarp <ruiizemmanuel@gmail.com>
2025-11-15 22:43:37 -0600

    Feat: App Theme colors setup

---
+++ lib/src/presentation/config/themes/dark_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DarkTheme {
+  static const Color backgroundColor = Color(0xFF212121);
+  static const Color cardBackgroundColor = Color(0xFF121212);
+
+  static const Color primaryColor = Color(0xFFFE6D73);
+  static const Color primaryColorDark = Color(0xFFA4133C);
+  static const Color primaryColorLight = Color(0xFFFFF0F3);
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
+  static const Color primaryColorFixed = Color(0xFFB91C1C);
+  static const Color primaryColorDim = Color(0xFF9F1239);
+  static const Color inversePrimaryColor = Color(0xFFB91C1C);
+
+  static const Color secondaryColor = Color(0xFF227C9D);
+  static const Color secondaryColorDark = Color(0xFF1E3A8A);
+  static const Color secondaryColorLight = Color(0xFFCAF0F8);
+  static const Color onSecondaryColor = Color(0xFFFFFFFF);
+  static const Color secondaryColorFixed = Color(0xFF1E40AF);
+  static const Color secondaryColorDim = Color(0xFF1E3A8A);
+
+  static const Color tertiaryColor = Color(0xFFFFCB77);
+  static const Color tertiaryColorDark = Color(0xFFB45309);
+  static const Color tertiaryColorLight = Color(0xFFFFFAE5);
+  static const Color onTertiaryColor = Color(0xFFFFFFFF);
+  static const Color tertiaryColorFixed = Color(0xFFB45309);
+  static const Color tertiaryColorDim = Color(0xFF92400E);
+
+  static const Color titleTextColor = Color(0xFFF9FAFB);
+  static const Color subtitleTextColor = Color(0xFFE5E7EB);
+  static const Color primaryTextColor = Color(0xFFD1D5DB);
+  static const Color secondaryTextColor = Color(0xFF9CA3AF);
+  static const Color disabledTextColor = Color(0xFF6B7280);
+}
