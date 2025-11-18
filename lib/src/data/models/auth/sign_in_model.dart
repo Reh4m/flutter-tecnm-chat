b07b847f8850631a887ed3bab7f8fa8472d20bfc
emmarp <ruiizemmanuel@gmail.com>
2025-11-18 03:50:06 -0600

    Refactor (AuthenticationModule): Remove 'SignIn' entity and model

--- lib/src/data/models/auth/sign_in_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_whatsapp_clon/src/domain/entities/auth/sign_in_entity.dart';
-
-class SignInModel extends SignInEntity {
-  const SignInModel({required super.email, required super.password});
-}
